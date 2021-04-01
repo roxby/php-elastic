15c5c9634ae4bfa53c5e2bce04147c50b9562500
juggernautt <polina.stavro@gmail.com>
2021-04-01 14:51:10 +0300

    add response class

---
+++ src/Response.php
@@ -0,0 +1,20 @@
+<?php
+namespace Roxby\Elastic;
+
+class Response
+{
+    public static function success($data = null) :array
+    {
+        $response = ["success" => true];
+        if(!is_null($data)) $response["data"] = $data;
+        return $response;
+    }
+
+    public static function error(\Exception $exception) :array
+    {
+        return [
+            "success" => true,
+            "error" => $exception->getMessage()
+        ];
+    }
+}
